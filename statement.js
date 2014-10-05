511893ff6a0d55e275edf6c5c91c5103ed678124
Steve Thomson <organicpanda@gmail.com>
2014-10-05 11:05:36 +0100

    Categories

--- statement.js
+++ statement.js
@@ -1,13 +1,34 @@
-var GBP = function(amount) {
-  amount = String(amount).replace('.', '');
-  this.pence = parseInt(amount, 10);
-}
+var ent = require('ent');
 
-var parseAmount = function(amount) {
-  return new GBP(amount);
+var GBP = function(amount) {
+  if (typeof amount == 'string') {
+    if (amount.indexOf('.') > -1) {
+      this.pence = parseInt(amount.replace('.', ''), 10);
+    } else {
+      this.pence = parseInt(amount, 10) * 100;
+    }
+  } else {
+    this.pence = amount;
+  }
 };
 
-var findSections = function(data) {
+GBP.prototype.add = function(a, b) {
+  return new GBP(a.pence + b.pence);
+};
+
+var parseDate = function(str) {
+  // 27/09/2013
+  var parse = /(\d{2})\/(\d{2})\/(\d{4})/
+    , dateArray = parse.exec(str); 
+  
+  return dateObject = new Date(
+    (+dateArray[3]),
+    (+dateArray[2]) - 1, // Careful, month starts at 0!
+    (+dateArray[1])
+  );
+};
+
+var findTransactions = function(data) {
   var newLine = '\\r\\n'
     , newLineOrSpace = newLine + '\\s'
     , value = '\\s([^' + newLine + ']+)'
@@ -19,10 +40,10 @@ var findSections = function(data) {
     , pattern = new RegExp(structure)
     , patterng = new RegExp(structure, 'g');
 
-  var sections = data.match(patterng) || [];
+  var transactions = data.match(patterng) || [];
 
-  return sections.map(function(section) {
-    var result = pattern.exec(section);
+  return transactions.map(function(transaction) {
+    var result = pattern.exec(transaction);
 
     if (result && result.length === 5) {
       var date = result[1].trim()
@@ -31,23 +52,47 @@ var findSections = function(data) {
         , balance = result[4].trim();
 
       return {
-        date: date,
-        description: description,
-        amount: parseAmount(amount),
-        balance: balance
+        date: parseDate(date),
+        description: ent.decode(description),
+        amount: new GBP(amount),
+        balance: new GBP(balance)
       };
     } else {
-      console.log('no match!', JSON.stringify(section));
+      console.log('no match!', JSON.stringify(transaction));
 
       return null;
     }
-  }).filter(function(section) {
-    return section !== null;
+  }).filter(function(transaction) {
+    return transaction !== null;
   });
 };
 
-module.exports = function(data) {
-  var sections = findSections(data);
+var isCategory = function(transaction, category) {
+  return category.rules.some(function(rule) {
+    return transaction.description.match(rule);
+  });
+};
 
-  console.log('sections', sections);
+var categorise = function(transactions) {
+  var categories = [
+    { name: 'Rent', rules: [
+      /REFERENCE STEPHEN RENT/
+    ], transactions: [] }
+  ];
+
+  categories.forEach(function(category) {
+    category.transactions = transactions.filter(function(transaction) {
+      return isCategory(transaction, category);
+    });
+  });
+
+  return categories;
+}
+
+module.exports = function(data) {
+  var transactions = findTransactions(data);
+
+  var categories = categorise(transactions);
+
+  console.log('categories', categories);
 };
