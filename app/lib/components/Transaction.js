2277671e831f1d4a38b8defe05d69dbaea0990eb
Steve Thomson <organicpanda@gmail.com>
2015-06-16 19:38:51 +0100

    Fixing React spacing

--- app/lib/components/Transaction.js
+++ app/lib/components/Transaction.js
@@ -2,11 +2,22 @@ import { el, $, Component } from '../react-utils';
 
 export class Transaction extends Component {
   render() {
-    return el('div', { className: 'transaction' },
-      el('span', { className: 'transaction-date' }, this.props.date.toString()),
-      el('span', { className: 'transaction-description' }, this.props.description),
-      el('span', { className: 'transaction-amount' }, this.props.amount.pence),
-      el('span', { className: 'transaction-balance' }, this.props.balance.pence)
+    return el('p', { className: 'transaction' },
+      el('span', {
+        className: 'label label-success transaction-date'
+      }, this.props.date.toString()),
+      ' ', // React LOL
+      el('span', {
+        className: 'transaction-description'
+      }, this.props.description),
+      ' ',
+      el('span', {
+        className: 'label label-success transaction-amount'
+      }, this.props.amount.pence),
+      ' ',
+      el('span', {
+        className: 'label label-success transaction-balance'
+      }, this.props.balance.pence)
     );
   }
 }
